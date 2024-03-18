a603a409c968df6564cde8e54545b1a43c94a396
nesrine_chiba <nessrine.chiba2@student-cs.fr>
2024-03-18 20:26:47 +0100

    Amelioration du code

--- Monte_carlo.py
+++ Monte_carlo.py
@@ -1,8 +1,9 @@
 import numpy as np 
+import matplotlib.pyplot as plt 
 
     
 class MonteCarlo_simulator(): 
-    def __init__(self,r,sigma,time_step,call = True,S0,L,n):
+    def __init__(self,r,sigma,S0,L,n):
         """
         ARGS: 
         r : risk free rate 
@@ -15,7 +16,6 @@ class MonteCarlo_simulator():
         """
         self.r= r 
         self.sigma = sigma
-        self.time_step = time_step
         self.S0 = S0
         self.L = L 
         self.n = n 
@@ -26,7 +26,7 @@ class MonteCarlo_simulator():
         for path in range(self.n):
             # We initialize the price at time 1 
             z_0 = np.random.normal(0,1)
-            Price_simulation[0,path] = self.s_0* np.exp((self.r-self.sigma**2/2)+self.sigma*z_0)
+            Price_simulation[0,path] = self.S0* np.exp((self.r-self.sigma**2/2)+self.sigma*z_0)
             for i in range(1,self.L): 
                 z_i = np.random.normal(0,1)
                 Price_simulation[i,path] = Price_simulation[i-1,path]*np.exp((self.r-self.sigma**2/2)+self.sigma*z_i)
@@ -34,11 +34,23 @@ class MonteCarlo_simulator():
         
         return Price_simulation
     
+    def visualisation_price(self,Price_simulation): 
+        plt.figure(figsize=(10, 6))
+        for i in range(self.n):
+            price = np.insert(Price_simulation[:, i],0,self.S0)
+            plt.plot(range(self.L+1), price, label=f"Path {i+1}")
+
+        plt.title('Monte Carlo Simulation of Asset Prices')
+        plt.xlabel('Time')
+        plt.ylabel('Price')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
 
     
-    def monte_carlo_payoff_simulation(self, payoff_function):
+    def monte_carlo_payoff_simulator(self, payoff_function):
 
-        price_simulation = MonteCarlo_simulator(self)
+        price_simulation = self.monte_carlo_price_simulator()
 
         return np.vectorize(payoff_function)(price_simulation)
     
