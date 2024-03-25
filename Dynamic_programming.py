432e086ed788ca73e7c936683d59fd45ba8de16e
ChafikHala <hala.chafik@student-cs.fr>
2024-03-25 13:00:39 +0100

    Trying

--- Dynamic_programming.py
+++ Dynamic_programming.py
@@ -16,7 +16,7 @@ class Dynamic_pricing(MonteCarlo_simulator):
     
 
     def least_square_minimizer(self, payoff_simulation, Tau_i_1, Price_simulation_i, Projection_base):
-        Y = np.array([payoff_simulation[int(Tau_i_1[path] - 1), path] for path in range(self.n)])
+        Y = np.array([payoff_simulation[Tau_i_1[path], path] for path in range(self.n)])
         X = np.array([Projection_base(self.m, Price_simulation_i[path]) for path in range(self.n)])
         model = LinearRegression()
         model.fit(X, Y)
@@ -32,9 +32,9 @@ class Dynamic_pricing(MonteCarlo_simulator):
         price_simulation=self.monte_carlo_price_simulator()
         payoff_simulation = self.monte_carlo_payoff_simulator(self.payoff_function)
 
-        Tau = np.zeros((L - 1, n))
-        Tau[L - 2, :] = L * np.ones(n)
-        for i in range(L - 3, -1, -1):
+        Tau = np.zeros((L, n))
+        Tau[L - 1, :] = L * np.ones(n)
+        for i in range(L - 2, -1, -1):
             alpha_i = self.least_square_minimizer(payoff_simulation, Tau[i + 1, :], price_simulation[i, :], self.Projection_base)
             for path in range(n):
                 approx_ = alpha_i.T @ self.Projection_base(m,price_simulation[i, path])
@@ -43,7 +43,7 @@ class Dynamic_pricing(MonteCarlo_simulator):
                 else:
                     Tau[i, path] = Tau[i + 1, path]
 
-        monte_carlo_approx = sum([payoff_simulation[int(Tau[0, i]-1),0] for i in range(n)]) / n
+        monte_carlo_approx = sum([payoff_simulation[Tau[0, i],i] for i in range(n)]) / n
         U_0 = max(payoff_0, monte_carlo_approx)
 
         return U_0
